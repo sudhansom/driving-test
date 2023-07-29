af5d651c78280ee893c2854f2eb1db98e06c02e2
bhuwan <bkspoudel@gmail.com>
2023-07-29 08:24:38 +0200

    fix the result page

--- src/app/views/test-page/test-page.component.ts
+++ src/app/views/test-page/test-page.component.ts
@@ -7,7 +7,8 @@ type IData = {
   options: {
     question: string,
     answer: boolean,
-    givenAnswer?: boolean
+    givenAnswer?: boolean,
+    reason?: string
   }[]
 }
 
@@ -23,11 +24,149 @@ export class TestPageComponent implements OnInit {
 
   wrongAnswers = 0;
 
+  finished = true;
+
 
   count = -1;
   currentData: any;
 
-  allAnswers: IData[] = []
+  allAnswers: IData[] = [
+    {
+      image: '../../../assets/images/image1.png',
+      explanation: ' What are the rules about warning triangle, in this situation?',
+      options: [
+        {
+          question: 'A warning triangle has to be placed 30 meters from the car.',
+          answer: true,
+          givenAnswer: true,
+          reason: 'because ...........'
+        },
+        {
+          question: 'A warning triangle has to be placed 60 meters from the car.',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'A warning triangle has to be placed 80 meters from the car.',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'A warning triangle has to be placed 100 meters from the car.',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        }
+      ]
+    },
+    // {
+    //   image: '../../../assets/images/image3.png',
+    //   explanation: ' You are driving 30 km/h. How will you continue?',
+    //   options: [
+    //     {
+    //       question: 'I pay special attention to the road work?',
+    //       answer: true,
+
+    //     },
+    //     {
+    //       question: 'I continue with the same or almost the same speed of 30 km/h?',
+    //       answer: false,
+    //     },
+    //     {
+    //       question: 'I break?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I pay special attention to the white road markings?',
+    //       answer: false,
+    //     }
+    //   ]
+    // },
+    // {
+    //   image: '../../../assets/images/image2.png',
+    //   explanation: ' You are staying by the curb and would like to set off. What will you do?',
+    //   options: [
+    //     {
+    //       question: 'I carefully set off straight away?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I make orientation in the blind angles, behind on my left?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'If there is no traffic, I will set off?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I wait a bit more to set off?',
+    //       answer: false,
+    //     }
+    //   ]
+    // },
+    {
+      image: '../../../assets/images/image4.png',
+      explanation: ' You are driving at about 70 km/h. There is no traffic from behind. How will you continue?',
+      options: [
+        {
+          question: 'I cross broken lines in the middle of the road to flatten the bend?',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I move close to the right edge of the road?',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I stay in the middle of my lane?',
+          answer: true,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I slow down?',
+          answer: true,
+          givenAnswer: false,
+          reason: 'because ...........'
+        }
+      ]
+    },
+    {
+      image: '../../../assets/images/image5.png',
+      explanation: ' You are driving at about 70 km/h. How will you continue?',
+      options: [
+        {
+          question: 'I continue at the same speed of 70 km/h?',
+          answer: false,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I slow down and continue at the speed of about 50 km/h?',
+          answer: true,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I pay special attention to the road surroundings? (condition of the road)',
+          answer: true,
+          givenAnswer: false,
+          reason: 'because ...........'
+        },
+        {
+          question: 'I pay special attention to the course of the road?',
+          answer: true,
+          givenAnswer: false,
+          reason: 'because ...........'
+        }
+      ]
+    },
+  ];
 
   showNextButton = false;
 
@@ -58,50 +197,50 @@ export class TestPageComponent implements OnInit {
         }
       ]
     },
-    {
-      image: '../../../assets/images/image3.png',
-      explanation: ' You are driving 30 km/h. How will you continue?',
-      options: [
-        {
-          question: 'I pay special attention to the road work?',
-          answer: true,
-        },
-        {
-          question: 'I continue with the same or almost the same speed of 30 km/h?',
-          answer: false,
-        },
-        {
-          question: 'I break?',
-          answer: true,
-        },
-        {
-          question: 'I pay special attention to the white road markings?',
-          answer: false,
-        }
-      ]
-    },
-    {
-      image: '../../../assets/images/image2.png',
-      explanation: ' You are staying by the curb and would like to set off. What will you do?',
-      options: [
-        {
-          question: 'I carefully set off straight away?',
-          answer: true,
-        },
-        {
-          question: 'I make orientation in the blind angles, behind on my left?',
-          answer: true,
-        },
-        {
-          question: 'If there is no traffic, I will set off?',
-          answer: true,
-        },
-        {
-          question: 'I wait a bit more to set off?',
-          answer: false,
-        }
-      ]
-    },
+    // {
+    //   image: '../../../assets/images/image3.png',
+    //   explanation: ' You are driving 30 km/h. How will you continue?',
+    //   options: [
+    //     {
+    //       question: 'I pay special attention to the road work?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I continue with the same or almost the same speed of 30 km/h?',
+    //       answer: false,
+    //     },
+    //     {
+    //       question: 'I break?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I pay special attention to the white road markings?',
+    //       answer: false,
+    //     }
+    //   ]
+    // },
+    // {
+    //   image: '../../../assets/images/image2.png',
+    //   explanation: ' You are staying by the curb and would like to set off. What will you do?',
+    //   options: [
+    //     {
+    //       question: 'I carefully set off straight away?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I make orientation in the blind angles, behind on my left?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'If there is no traffic, I will set off?',
+    //       answer: true,
+    //     },
+    //     {
+    //       question: 'I wait a bit more to set off?',
+    //       answer: false,
+    //     }
+    //   ]
+    // },
     {
       image: '../../../assets/images/image4.png',
       explanation: ' You are driving at about 70 km/h. There is no traffic from behind. How will you continue?',
@@ -185,8 +324,9 @@ export class TestPageComponent implements OnInit {
 
         })
       })
+      console.log('right answers:', this.allAnswers.length - this.wrongAnswers);
+      this.finished = true;
     }
-    console.log('right answers:', this.allAnswers.length - this.wrongAnswers);
   }
 
   ngOnInit(): void {

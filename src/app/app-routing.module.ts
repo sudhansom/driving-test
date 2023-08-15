a3fee56bbda4b25efee677838bb611726b5ce2ed
bhuwan <bkspoudel@gmail.com>
2023-08-15 09:34:15 +0200

    add one more container to practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'css-before', loadChildren: () => import('./views/css-before/css-before.module').then((mod)=>mod.CssBeforeModule)},
   { path: '', loadChildren: () => import('./views/test-page/test-page.module').then((mod) => mod.TestModule)},
   { path: 'linked-list', loadChildren: () => import('./views/linked-list/linked-list.module').then((mod) => mod.InputFormModule)},
+  {path: 'grid', loadChildren: () => import('./views/display-grid/display-grid.module').then((mod)=>mod.DisplayGridModule)},
 ];
 
 @NgModule({

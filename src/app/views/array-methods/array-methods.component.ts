3b29956bfd7c6d1fa53514dfc76ee529484c21f3
bhuwan <bkspoudel@gmail.com>
2023-08-21 19:39:13 +0200

    implement push method

--- src/app/views/array-methods/array-methods.component.ts
+++ src/app/views/array-methods/array-methods.component.ts
@@ -8,9 +8,14 @@ import { data } from 'src/app/data';
   selector: 'app-array-methods.',
   templateUrl: './array-methods.component.html',
   styleUrls: ['./array-methods.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArrayMethodsComponent {
   myArray: string[] = ['a', 'b', 'c', 'd', 'e'];
+  originalArray: string[] = [...this.myArray];
+
+  pushItem(){
+    this.myArray.push('new');
+  }
 }
 

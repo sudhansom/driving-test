a685287ca80ae80ae45f90125d635c6cbee1390b
bhuwan <bkspoudel@gmail.com>
2023-08-21 22:20:16 +0200

    save changes

--- src/app/views/array-methods/array-methods.component.ts
+++ src/app/views/array-methods/array-methods.component.ts
@@ -1,14 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal } from '@angular/core';
-import { PlatformLocation } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { IData } from 'src/app/types';
-import { data } from 'src/app/data';
 
 @Component({
   selector: 'app-array-methods.',
   templateUrl: './array-methods.component.html',
   styleUrls: ['./array-methods.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArrayMethodsComponent {
   myArray: string[] = ['a', 'b', 'c', 'd', 'e'];
